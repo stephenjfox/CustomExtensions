1bef6566f8b09a6e077fd1a03a426ae0abb3283a
Stephen Fox <stevethecoder34@gmail.com>
2015-10-14 23:03:17 -0600

    CollectionExtensions.castBetter() has received a hefty improvement. See Javadoc.
    MathExtension was a rename and recieved Random aliasing furctionality.

--- src/com/fox/collections/CollectionExtension.java
+++ src/com/fox/collections/CollectionExtension.java
@@ -1,8 +1,8 @@
 package com.fox.collections;
 
-import com.fox.io.log.ConsoleLogger;
 import com.fox.types.Classes;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Collection;
@@ -11,6 +11,8 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import static com.fox.io.log.ConsoleLogger.debugFormatted;
+import static com.fox.io.log.ConsoleLogger.exception;
 import static com.google.common.base.Preconditions.checkState;
 
 /**
@@ -129,22 +131,65 @@ public class CollectionExtension {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Performs an O(n) - where 'n' is the element count of the passed collection - casting of the
+     * elements into another type lens
+     * Ex:
+     * // This runs, given HistoricEvent matches the Event constructor
+     * {@code
+     * ArrayDequeue<Event> events = EventQueueFactory.foo();
+     * Queue<HistoricEvent> histories = CollectionExtension.castBetter(events);
+     * }
+     *
+     * In the future, I want this to be able to match constructors reflectively and be able to match
+     * the type off of param <C> instead.
+     *
+     * Future ex:
+     *
+     * // shout out to Google's Guava library "collections" package
+     * {@code
+     * ImmutableList<Integer> integers = ImmutableList.of(1,2,3,4);
+     * RegularImmutableList<Integer> regularInts = CollectionExtension.castBetter(integers);
+     * // of better yet...
+     * RegularImmutableList<Long> regularInts = CollectionExtension.castBetter(integers);
+     * // because, if we can, we SHOULD
+     * }
+     *
+     * @param collection Some generic collection
+     * @param <E> type in passed collection
+     * @param <T> some child
+     * @param <C> Some collection of that child, where super/interface types perform more reliably
+     * @return newInstance of the passed collection through the lens of param {@code <C>}
+     */
     public static <E, T extends E, C extends Collection<T>> C castBetter( Collection<E> collection ) {
 //        Type preliminarySuperType = Classes.getPreliminarySuperType(collection.getClass());
         try {
-            return (C) Classes.dotClass(collection).getConstructor(Collection.class).newInstance(collection);
+            Class<Object> objectClass = Classes.dotClass(collection.getClass());
+            debugFormatted("collection.class: %s", objectClass);
+
+            Constructor<Object> constructor = objectClass.getConstructor(Collection.class);
+            debugFormatted("Got ctor: %s", constructor);
+            debugFormatted("Collection<E>.preliminarySuperType()", Classes.getPreliminarySuperType(collection.getClass()));
+
+            return (C) constructor.newInstance(collection);
         }
         catch (NoSuchMethodException e) {
-            ConsoleLogger.exception(e, "No method could be found");
+            exception(e, "No method could be found");
         }
         catch (InvocationTargetException e) {
-            ConsoleLogger.exception(e, "Invocation target was invalid... Probably");
+            exception(e, "Invocation target was invalid... Probably");
         }
         catch (InstantiationException e) {
-            ConsoleLogger.exception(e, "Instantiation exception... Whatever that is\n");
+            exception(e, "Instantiation exception... Whatever that is\n");
         }
         catch (IllegalAccessException e) {
-            ConsoleLogger.exception(e, "Constructor was inaccessible");
+            exception(e, "Constructor was inaccessible");
+        }
+        catch (NullPointerException e) {
+            exception(e, "Nullpointer on objectClass (see code for more)\n");
+        }
+        catch (ClassNotFoundException e) {
+            exception(e, "ClassNotFound from Classes.dotClass(collection.getClass())");
         }
         return null;
     }
---
+++ src/com/fox/util/math/MathExtension.java
@@ -0,0 +1,43 @@
+package com.fox.util.math;
+
+import java.util.Random;
+
+/**
+ * Created by stephen on 5/26/15.
+ */
+public class MathExtension {
+
+    private static Random rGen = new Random();
+
+    /**
+     * Computes the complimentary angle for the input
+     * @param angleA
+     * @return the angle that would sum the angle
+     */
+    public static double compAngle( double angleA ) {
+
+        if (angleA > 90) angleA -= 90;
+
+        return 90 - angleA;
+    }
+
+    public static void seedRandom(long seed) {
+        rGen = new Random(seed);
+    }
+
+    public static int randomInt( int lowerBound, int upperBound ) {
+        return randomInt(lowerBound, upperBound, true);
+    }
+
+    public static int randomInt( int lowerBound, int upperBound, boolean upperInclusive ) {
+        return randomInt(lowerBound, upperBound, true, upperInclusive);
+    }
+
+    public static int randomInt( int lowerBound, int upperBound, boolean lowerInclusive, boolean upperInclusive ) {
+        int low = lowerInclusive ? lowerBound : lowerBound + 1;
+        int high = upperInclusive ? upperBound + 1 : upperBound;
+
+        return rGen.nextInt(high - low) + low;
+    }
+
+}
