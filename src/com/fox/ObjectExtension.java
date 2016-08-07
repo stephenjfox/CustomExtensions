35a432a545cbcd24c554f5b03c2f0e1b26445169
Stephen Fox <stevethecoder34@gmail.com>
2016-08-06 20:27:58 -0600

    Start implementing for #3

--- src/com/fox/ObjectExtension.java
+++ src/com/fox/ObjectExtension.java
@@ -9,36 +9,40 @@ import java.lang.reflect.Field;
  */
 public class ObjectExtension {
 
+  /**
+   * This function is meant to replicate the following C# code:
+   foreach (var prop in o.GetType().GetProperties())
+   {
+     Console.WriteLine("Property type before 'for' loop {0}", prop.GetType());
+     Console.WriteLine("Property declaring type: " + prop.DeclaringType);
+     if (prop.GetValue(o) is IDictionary<object, object>)
+     {
+       var tempDict = prop.GetValue(o) as IDictionary<string, int>;
+       builder.AppendFormat("Character {0}: {1}\n", prop.Name, prop.GetValue(o));
+       foreach (var item in tempDict)
+       {
+        builder.AppendFormat("\tKey: {0}, Value: {1}\n ", item.Key, item.Value);
+       }
+     }
+     else
+     {
+      //Debug.Write("Character " + prop.Name + ": " + prop.GetValue(o));
+      builder.AppendFormat("Character {0}: {1}\n", prop.Name, prop.GetValue(o));
+     }
+   }
+   return builder.ToString();
+   *
+   * @param o
+   * @return
+   */
   public static String propertyToString(Object o) {
     StringBuilder builder = new StringBuilder();
 
     // Dependencies and the things we need to do.
 
-        /*
-         foreach (var prop in o.GetType().GetProperties())
-            {
-                Console.WriteLine("Property type before 'for' loop {0}", prop.GetType());
-                Console.WriteLine("Property declaring type: " + prop.DeclaringType);
-                if (prop.GetValue(o) is IDictionary<object, object>)
-                {
-                    var tempDict = prop.GetValue(o) as IDictionary<string, int>;
-                    builder.AppendFormat("Character {0}: {1}\n", prop.Name, prop.GetValue(o));
-                    foreach (var item in tempDict)
-                    {
-                        builder.AppendFormat("\tKey: {0}, Value: {1}\n ", item.Key, item.Value);
-                    }
-                }
-                else
-                {
-                    //Debug.Write("Character " + prop.Name + ": " + prop.GetValue(o));
-                    builder.AppendFormat("Character {0}: {1}\n", prop.Name, prop.GetValue(o));
-                }
-            }
-         */
-
     for (Field field : o.getClass().getFields()) {
 
-      ConsoleLogger.writeLineFormatted("Property type at top of for-loop: %s", field.getType());
+      builder.append(String.format("Property type at top of for-loop: %s", field.getType()));
     }
 
     return builder.toString();
